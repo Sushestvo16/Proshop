fc1b1329237b037d11791ed0c62f73cb534a184f
Sushestvo16 <Sushestvo16@gmail.com>
2020-11-08 20:22:01 +0200

    new commit

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,18 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+
 
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            const res = await axios.get('/api/products')
-            setProducts(res.data)
-        }
-        fetchProducts()
+
     }, [])
 
     return (
